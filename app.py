c23587c6885191c218bc9ee50988e8c1bb0c9929
Jasvik999 <hitesh.vidhani@gmail.com>
2026-06-22 01:55:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+import smtplib
+from email.message import EmailMessage
 
+msg = EmailMessage()
+msg["Subject"] = "Test Email"
+msg["From"] = "your_email@example.com"
+msg["To"] = "recipient@example.com"
+msg.set_content("Hello from Python!")
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+    smtp.login("your_email@example.com", "app_password")
+    smtp.send_message(msg)
+
+print("Email sent!")
